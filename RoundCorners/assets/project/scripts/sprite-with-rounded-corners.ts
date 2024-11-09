3df73dce73f9f5a18e9deb6f7d273666bace7586
Bisenbenov Danabek <bisenbenov@tiltshift.xyz>
2024-11-10 00:24:06 +0700

    - feat: version 0.0.1

---
+++ RoundCorners/assets/project/scripts/sprite-with-rounded-corners.ts
@@ -0,0 +1,83 @@
+import {_decorator, CCFloat, Component, Material, Sprite, UITransform, Vec2, assetManager, EffectAsset, resources, Texture2D, SpriteFrame} from 'cc';
+
+class Constants {
+    public static SPRITE_TEXTURE: string = "spriteTexture";
+    public static RADIUS: string = "radius";
+    public static SIZE: string = "size";
+}
+
+@_decorator.ccclass('SpriteWithRoundedCorners')
+@_decorator.executeInEditMode
+export class SpriteWithRoundedCorners extends Component {
+    @_decorator.property private _radius: number = 0;
+    @_decorator.property({type: CCFloat})
+    get radius(): number {
+        return this._radius;
+    }
+    set radius(value: number) {
+        this._radius = value;
+        this.updateProperties();
+    }
+
+    private declare _sprite: Sprite;
+    private declare _material: Material;
+    private declare _uiTransform: UITransform;
+
+    protected onLoad(): void {
+        this._sprite = this.getComponent(Sprite);
+        this._uiTransform = this.getComponent(UITransform);
+
+        this.validate();
+    }
+
+    private validate(): void {
+        // Well, that's pretty lame, but it's the best way to do it for now
+        // (so you can use this component in the edit mode)
+
+        // All you have to do is copy the uuid from the effect and paste it here:
+        const uuid: string = "66faad1e-c36c-4c15-ad33-723feba537b3";
+
+        let effect: EffectAsset = new EffectAsset();
+
+        assetManager.loadAny({uuid: uuid, type: EffectAsset}, (err, effectAsset) => {
+            if (err) {
+                console.warn("Cannot load effect!");
+                return;
+            }
+
+            effect = effectAsset;
+
+            this._material = new Material();
+            this._material.reset({ effectAsset: effect });
+
+            this._sprite.setSharedMaterial(this._material, 0);
+            this._sprite.customMaterial = this._material;
+
+            this._sprite.getMaterialInstance(0).recompileShaders({ USE_LOCAL: true, USE_TEXTURE: true });
+
+            this.updateProperties();
+        });
+    }
+
+    private updateProperties(): void {
+        if (!this._material) {
+            return;
+        }
+
+        const frame = this._sprite.spriteFrame;
+
+        if (!frame.texture) {
+            return;
+        }
+
+        const size = this._uiTransform.contentSize;
+        const sizeVector = new Vec2(size.x, size.y);
+
+        this._material.setProperty(Constants.SIZE, sizeVector);
+        this._material.setProperty(Constants.SPRITE_TEXTURE, frame.texture);
+        this._material.setProperty(Constants.RADIUS, this.radius);
+
+        this._sprite.setMaterialInstance(this._material, 0);
+        this._sprite.setSharedMaterial(this._material, 0);
+    }
+}
